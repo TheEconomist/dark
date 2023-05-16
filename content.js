8eeaec231b4435785ab4e509120ca179879a3dfd
fredchen <fredchen@economist.com>
2023-05-16 12:41:23 +0100

    tweak with injected css

--- content.js
+++ content.js
@@ -6,24 +6,8 @@ const r = document.querySelector(':root');
 r.style.setProperty("--ds-color-london-5", "white")
 r.style.setProperty("--ds-color-london-10", "white")
 
-// idk
+// masthead menu background color
+r.style.setProperty("--ds-color-london-20", "white")
+
+// box thingy
 r.style.setProperty("--ds-color-los-angeles-95", "transparent")
-
-// masthead popup background
-// r.style.setProperty("--ds-color-london-20", "white")
-// for (const e of document.getElementsByClassName("ds-masthead is-open")) {
-//     e.style.background = "grey"
-// }
-
-// masthead, masthead popup text
-// r.style.setProperty("--ds-color-london-100", "transparent")
-for (const e of document.getElementsByClassName("styled-masthead")) {
-    e.style.backgroundColor = "transparent"
-}
-
-// aside tag
-for (const e of document.getElementsByClassName("article__body-aside")) {
-    e.style.backgroundColor = "transparent"
-}
-
-document.body.style.backgroundColor = "black"
