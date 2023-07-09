4f39ea78a63bc2bfbcc9d332e36e44da1cc9ff60
Tq-bit <t.quante@outlook.com>
2023-07-09 21:10:01 +0200

    patch: change colors to be a bit easier on the eye - replace dark w/ gray - add second gray tone - apply white color to body element - change some svg el to be visible w/ new gray tone

--- content.js
+++ content.js
@@ -3,14 +3,14 @@
 const r = document.querySelector(':root');
 
 // text
-r.style.setProperty("--ds-color-london-5", "white")
-r.style.setProperty("--ds-color-london-10", "white")
+r.style.setProperty("--ds-color-london-5", "#f9f9f9")
+r.style.setProperty("--ds-color-london-10", "#f9f9f9")
 
 // masthead menu background color
-r.style.setProperty("--ds-color-london-20", "white")
+r.style.setProperty("--ds-color-london-20", "#f9f9f9")
 
 // box thingy
 r.style.setProperty("--ds-color-los-angeles-95", "transparent")
 
 // anchor hover
-r.style.setProperty("--ds-color-chicago-30", "white")
+r.style.setProperty("--ds-color-chicago-30", "#f9f9f9")
