bd0839cdc4ebf4070b02dac779870cd96bb8ca66
Tq-bit <t.quante@outlook.com>
2023-07-11 15:48:58 +0200

    feat: add color opts - colors for buttons, cta, search form - adjustments to share modal and anchor hover

--- content.js
+++ content.js
@@ -12,8 +12,14 @@ r.style.setProperty("--ds-color-london-20", "#f9f9f9")
 // special article banner
 r.style.setProperty("--ds-color-london-95", "#0d0d0d")
 
+// Share modal
+r.style.setProperty('--ds-color-london-100', '#0d0d0d')
+
 // box thingy
 r.style.setProperty("--ds-color-los-angeles-95", "transparent")
 
 // anchor hover
-r.style.setProperty("--ds-color-chicago-30", "#f9f9f9")
+r.style.setProperty("--ds-color-chicago-30", "#f9f9f9");
+
+// Underlined text and buttons
+r.style.setProperty("--ds-color-chicago-45", "#1a2c78");
