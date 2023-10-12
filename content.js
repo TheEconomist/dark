c375dde347bc5e104e457d403002014bd4affa6f
tq-bit <69204418+tq-bit@users.noreply.github.com>
2023-10-12 17:48:43 +0200

    patch: change colors to be a bit easier on the eye (#1)
    * patch: change colors to be a bit easier on the eye
    - replace dark w/ gray
    - add second gray tone
    - apply white color to body element
    - change some svg el to be visible w/ new gray tone
    
    * invert default bright/dark colors
    
    * feat: invert color for la-95
    
    * feat: add color scheme selector
    
    * feat: add color opts
    - colors for buttons, cta, search form
    - adjustments to share modal and anchor hover
    
    * fix: missing important ds-button
    
    * fix: missing colors for menu
    
    * Add files via upload
    
    * Add files via upload
    
    * Update README.md
    
    * Update README.md
    
    * Revert "Add files via upload"
    
    This reverts commit 79a18831b974b34a41486ae206547d8117e0b7f3.
    
    * Revert "Add files via upload"
    
    This reverts commit d55b524454a4c69b48f94b8d285b20011d33baa8.
    
    * Revert "Update README.md"
    
    This reverts commit 114aa0fd7bc64ab03d6c48ce1637fd87dfdf777a.
    
    * Revert "Update README.md"
    
    This reverts commit a9a73c2d03e80ca3a5807b669bb62038572bdb0b.
    
    ---------
    
    Co-authored-by: contrasting <40671177+contrasting@users.noreply.github.com>

--- content.js
+++ content.js
@@ -3,17 +3,23 @@
 const r = document.querySelector(':root');
 
 // text
-r.style.setProperty("--ds-color-london-5", "white")
-r.style.setProperty("--ds-color-london-10", "white")
+r.style.setProperty("--ds-color-london-5", "#f9f9f9")
+r.style.setProperty("--ds-color-london-10", "#f9f9f9")
 
 // masthead menu background color
-r.style.setProperty("--ds-color-london-20", "white")
+r.style.setProperty("--ds-color-london-20", "#f9f9f9")
+
+// special article banner
+r.style.setProperty("--ds-color-london-95", "#0d0d0d")
+
+// Share modal
+r.style.setProperty('--ds-color-london-100', '#0d0d0d')
 
 // box thingy
 r.style.setProperty("--ds-color-los-angeles-95", "transparent")
 
 // anchor hover
-r.style.setProperty("--ds-color-chicago-30", "white")
+r.style.setProperty("--ds-color-chicago-30", "#f9f9f9");
 
-// special report menu
-r.style.setProperty("--ds-color-london-95", "black")
+// Underlined text and buttons
+r.style.setProperty("--ds-color-chicago-45", "#1a2c78");
