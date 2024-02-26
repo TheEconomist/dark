d7203a8a0b5bbb5b7a64a31b56492d1a99617dd1
contrasting <captainhacker@hotmail.co.uk>
2024-02-26 09:22:43 +0000

    change white colour hex (#4)

--- content.js
+++ content.js
@@ -3,11 +3,11 @@
 const r = document.querySelector(':root');
 
 // text
-r.style.setProperty("--ds-color-london-5", "#f9f9f9")
-r.style.setProperty("--ds-color-london-10", "#f9f9f9")
+r.style.setProperty("--ds-color-london-5", "#eeeeee")
+r.style.setProperty("--ds-color-london-10", "#eeeeee")
 
 // masthead menu background color
-r.style.setProperty("--ds-color-london-20", "#f9f9f9")
+r.style.setProperty("--ds-color-london-20", "#eeeeee")
 
 // special article banner
 r.style.setProperty("--ds-color-london-95", "#0d0d0d")
@@ -19,7 +19,7 @@ r.style.setProperty('--ds-color-london-100', '#0d0d0d')
 r.style.setProperty("--ds-color-los-angeles-95", "transparent")
 
 // anchor hover
-r.style.setProperty("--ds-color-chicago-30", "#f9f9f9");
+r.style.setProperty("--ds-color-chicago-30", "#eeeeee");
 
 // Underlined text and buttons
 r.style.setProperty("--ds-color-chicago-45", "#1a2c78");
