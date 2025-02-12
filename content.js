1a043e2b42a21fa58c9f83f01dafa13862e980e1
fredchen <fredchen@economist.com>
2025-02-12 15:53:01 +0000

    fix: masthead

--- content.js
+++ content.js
@@ -7,8 +7,8 @@ r.style.setProperty("--mb-colour-greyscale-london-5", "#eeeeee")
 r.style.setProperty("--ds-color-london-5", "#eeeeee")
 // r.style.setProperty("--ds-color-london-10", "#eeeeee")
 
-// masthead menu background color
-r.style.setProperty("--ds-color-london-20", "#eeeeee")
+// masthead
+r.style.setProperty("--mb-colour-greyscale-london-100", "#0d0d0d")
 
 // special article banner
 r.style.setProperty("--ds-color-london-95", "#0d0d0d")
@@ -30,3 +30,20 @@ r.style.setProperty("--ds-type-system-serif", "EconomistSansOsF")
 
 // anchor active background colour
 r.style.setProperty("--ds-color-chicago-95", "#transparent");
+
+/*
+(async () => {
+    r.style.setProperty("--ds-type-system-serif", "Roboto");
+    r.style.setProperty("--ds-type-system-serif-lining", "Roboto");
+    r.style.setProperty("--ds-type-system-sans", "Roboto");
+    r.style.setProperty("--ds-type-system-sans-lining", "Roboto");
+
+    const customFont = document.createElement("link");
+    customFont.href = "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap";
+    customFont.rel = "stylesheet";
+
+    while (document.head == null) await new Promise(resolve => setTimeout(resolve, 100));
+
+    document.head.appendChild(customFont);
+})()
+*/
